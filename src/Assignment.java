3fb3e56abfa870c9039a05445d82a27f3af5f8d0
Matthew Bennett <trashlinkmain@gmail.com>
2024-11-04 19:10:46 -0600

    added assigment to string

--- src/Assignment.java
+++ src/Assignment.java
@@ -4,24 +4,18 @@ public class Assignment {
     private String dueDate;
     private Course course;
 
-    // Constructor
     public Assignment(String assignmentID, String title, String dueDate, Course course) {
         this.assignmentID = assignmentID;
         this.title = title;
         this.dueDate = dueDate;
         this.course = course;
     }
-
-
     public String getAssignmentID() { return assignmentID; }
-    public void setAssignmentID(String assignmentID) { this.assignmentID = assignmentID; }
-
     public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
-
     public String getDueDate() { return dueDate; }
-    public void setDueDate(String dueDate) { this.dueDate = dueDate; }
-
     public Course getCourse() { return course; }
-    public void setCourse(Course course) { this.course = course; }
+
+    public String toString() {
+        return assignmentID + "," + title + "," + dueDate + "," + course.getCourseID();
+    }
 }
