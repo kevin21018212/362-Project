6e6d2809084d615cf16da521ba01f1e9503ce6eb
maxmayermader <maxmayermader2@gmail.com>
2024-11-15 17:55:05 -0600

    Revert "view courses for both student and instructor"
    This reverts commit 4981126625849415165f59ccc078579ae24de70c.
    
    revert 4981126

--- src/helpers/Display.java
+++ src/helpers/Display.java
@@ -38,7 +38,7 @@ public class Display {
 
             switch (choice) {
                 case "1":
-                    student.viewCourses();
+                    //display all courses
                     break;
                 case "2":
                     assert student != null;
@@ -94,14 +94,14 @@ public class Display {
 
         while (true) {
             displayMessage("\nInstructor Menu:");
-            displayMessage("1 View Courses");
+            displayMessage("1 Create Assignment");
             displayMessage("2 Grade Assignments");
             displayMessage("3 Logout");
             String choice = Utils.getInput("Select an option: \n");
 
             switch (choice) {
                 case "1":
-                    instructor.viewCourses();
+                    //create assigment
                     break;
                 case "2":
                     instructor.gradeAssignments();
