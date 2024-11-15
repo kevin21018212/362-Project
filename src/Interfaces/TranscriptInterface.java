bd5bb0408b4c1756ceb8f7620da38cec4dd2a67a
maxmayermader <maxmayermader2@gmail.com>
2024-11-15 17:49:16 -0600

    revert test

--- src/Interfaces/TranscriptInterface.java
+++ src/Interfaces/TranscriptInterface.java
@@ -3,5 +3,5 @@ package Interfaces;
 public interface TranscriptInterface {
     boolean generateTranscript(String studentId);
     float calculateGPA(String studentId);
-//    List<Course> getCompletedCourses(String studentId);
+//    List<Course> getCompletedCourses(String studentId); //
 }
